d80b8378f714a44450208858ccadb17a2a40db61
oksonkma <131204809+oksonkma@users.noreply.github.com>
2023-04-27 15:42:07 +0200

    Style and clock animations

---
+++ script.js
@@ -0,0 +1,34 @@
+const clockElement = document.getElementById('clock');
+let time = new Date().toLocaleTimeString();
+clockElement.innerText = time;
+
+function updateClock() {
+    let time = new Date().toLocaleTimeString();
+    clockElement.innerText = time;
+}
+setInterval(updateClock, 1000);
+
+const searchElem= document.getElementById('search') ;
+const searchFieldElem= document.getElementById('search-field') ;
+const searchString= "https://duckduckgo.com/?q="
+document.addEventListener("keydown",event=>{
+    if (event.key==" "){
+        searchFieldElem.value ='';
+        searchElem.style.display='flex';
+        searchFieldElem.focus();
+    }else if (event.key =="Escape"){
+        searchFieldElem.value ='';
+        searchFieldElem.blur();
+        searchElem.style.display='none';
+
+    }
+
+    document.addEventListener("keydown",event=>{
+        if(event.key=="Enter"){
+            let query= searchFieldElem.value;
+            window.open(searchString + query, "self");
+        }
+
+    });
+
+});
